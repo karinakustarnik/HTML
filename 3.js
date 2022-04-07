89a79d7ca932000ac1d9c9adb72eb61142cdb202
karinakustarnik <101788918+karinakustarnik@users.noreply.github.com>
2022-04-07 21:07:43 +0600

    Add files via upload

---
+++ 3.js
@@ -0,0 +1,12 @@
+var x = 23;
+switch (x) { 
+case 1: 
+console.log("Переменная равна 1");
+break; 
+case 56: 
+console.log("Переменная равна 56");
+break; 
+default:
+console.log("Что-то другое");
+break;
+}
---
+++ 4.js
@@ -0,0 +1,3 @@
+var x = 90, a = 8;
+var res = x < a ? (x + a) : (x - a);
+console.log(res);
